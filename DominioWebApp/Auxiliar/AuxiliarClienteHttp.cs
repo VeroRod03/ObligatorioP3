13f3d307df7595cc316f786c59d603eb7cdeeb8c
VeroRod03 <veronicarod2003@gmail.com>
2025-11-19 11:48:43 -0300

    Auxiliar Cliente Http clase

---
+++ DominioWebApp/Auxiliar/AuxiliarClienteHttp.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http.Headers;
+
+namespace WebAppClienteHttp.Auxiliares
+{
+    public class AuxiliarClienteHttp
+    {
+
+        public static HttpResponseMessage EnviarSolicitud(string url, string verbo, object obj = null, string token = null)
+        {
+            HttpClient cliente = new HttpClient();
+            cliente.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            Task<HttpResponseMessage> tarea = null;
+
+            if (verbo == "GET") {             
+                tarea = cliente.GetAsync(url);
+            }
+            else if (verbo == "POST") {
+                tarea = cliente.PostAsJsonAsync(url, obj);
+            }
+            else if (verbo == "DELETE")
+            {
+                tarea = cliente.DeleteAsync(url);
+            }
+            else if (verbo == "PUT")
+            {
+                cliente.PutAsJsonAsync(url, obj);
+            }
+
+            tarea.Wait();
+            return tarea.Result;
+        }
+
+
+        public static string ObtenerBody(HttpResponseMessage respuesta)
+        {
+            var tarea = respuesta.Content.ReadAsStringAsync();
+            tarea.Wait();
+            return tarea.Result;
+        }
+    }
+}
